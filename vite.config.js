4b28fe7a18472a76f2ad53e4b32cad40275700f8
Irving <irvingfsanchez@proton.me>
2025-02-01 23:43:05 -0600

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/syllabye-capstone/", // Match GitHub repo name
+  base: "/Syllabye-Capstone/", // Match GitHub repo name
 });
